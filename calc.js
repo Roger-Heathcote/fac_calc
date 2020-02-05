77d6bc95e4c9f29b900f796e37cc33c17d18229f
Roger Heathcote <git@technicalbloke.com>
2020-02-05 10:15:55 +0000

    Added some basic test cases and logic but it 's messy so refactoring next.

--- calc.js
+++ calc.js
@@ -1,16 +1,84 @@
 function NewCalc(){
 
+    const isOp = (character) => "+-*/".includes(character);
+    const isDigit = (character) => "0123456789.".includes(character);
+    
     class Calculator {
         constructor() {
-            this.expression = ""
+            this.isNum = false;
+            this.pointYet = false;
+            this.expression = [];
             this.accumulator = 0;
         }
         set clear(_) {
-            this.expression = ""
+            this.expression = [];
             this.accumulator = 0;
         }
+        get acc() {
+            return this.accumulator;
+        }
+        add_char(character) {
+            // Some character checking goes here
+            if(!isDigit(character)){
+                if(this.isNum){ // So we were parsing a number but now this isn't a number
+                    // If we are parsing a number stop and evaluate it
+                    let digits = [];
+                    while(isDigit(this.expression[this.expression.length-1])){
+                        digits.unshift( this.expression.pop() );
+                    }
+                    if(true){
+                        let oneBack = this.expression.length-1;
+                        let twoBack = oneBack - 1;
+                        if(oneBack >= 0 && this.expression[oneBack] == "-"){
+                            if(twoBack < 0 || isOp(this.expression[twoBack]) ){
+                                digits.unshift( this.expression.pop() );
+                            }
+                        }
+                    }
+                    let theNum = digits.join('');
+                    this.expression.push(Number(theNum));
+                    this.isNum = false;
+                    this.pointYet = false;
+                }
+                const lastChar = this.expression[this.expression.length-1]; 
+                // console.log("LASTCHAR IS:", lastChar); 
+                if(isOp(lastChar) && character !== "-") this.expression.pop()
+            }
+            if(character === '.' && this.pointYet) return false;
+            if(isDigit(character)){
+                this.isNum = true;
+                if(character === '.') this.pointYet = true;
+            }
+            this.expression.push(character);
+            // console.log( "EXPR:", this.expression )
+        }
+        evaluate() {
+            // console.log("INEVAL:", this.isNum)
+            if(this.isNum){
+                let digits = [];
+                while(isDigit(this.expression[this.expression.length-1])){
+                    // console.log("CONSOLIDATING");
+                    digits.unshift( this.expression.pop() );
+                }
+                if(true){
+                    let oneBack = this.expression.length-1;
+                    let twoBack = oneBack - 1;
+                    if(oneBack >= 0 && this.expression[oneBack] == "-"){
+                        if(twoBack < 0 || isOp(this.expression[twoBack]) ){
+                            digits.unshift( this.expression.pop() );
+                        }
+                    }
+                }                let theNum = digits.join('');
+                this.expression.push(Number(theNum));
+                this.accumulator = Number(this.expression[0]);
+            }
+        }
         get equals() {
-            return 0;
+            if(this.expression.length) this.evaluate();
+            return this.accumulator;
+        }
+        get display() {
+            return this.expression.join("");
         }
     }
 
--- test.js
+++ test.js
@@ -2,21 +2,111 @@ const fs = require('fs');
 const calc_class_text = fs.readFileSync('./calc.js','utf8');
 eval(calc_class_text);
 
+const CLEAR = "\x1b[0m"
+const BLACKONYELLOW = "\x1b[30m\x1b[43m";
+const GREEN = "\x1b[32m";
+const RED = "\x1b[31m"
+
 function test(msg,a,b){
-    if(a !== b){
-        console.log("FAIL:",msg);
-        console.log(a, "!==", b);
+    if(typeof(a) !== typeof(b)){
+        console.log(RED + "FAIL:", CLEAR + msg);
+        console.log("Types differ", typeof(a), "!==", typeof(b));                
+        return false
     }
-    else {
-        console.log("PASS:",msg)
+    // If it's a list
+    if(typeof(a) == typeof([])){
+        // Check lengths are equal
+        if(a.length !== b.length){
+            console.log(RED + "FAIL:", CLEAR + msg);
+            console.log("Length's differ", a.length, "!==", b.length);                
+            return false
+        }
+        // Check elements are equal
+        for(let idx=0; idx<a.length; idx++){
+            if(a[idx] !== b[idx]){
+                console.log(RED + "FAIL:", CLEAR + msg);
+                console.log("Element", idx,"differs", a[idx], "!==", b[idx]);                
+                return false;
+            }
+        }
+        console.log(GREEN + "PASS:", CLEAR + msg);
+        return true;
+    } else {
+        if(a !== b){
+            console.log(RED + "FAIL:", CLEAR + msg);
+            console.log(a, "!==", b);
+        }
+        else {
+            console.log(GREEN + "PASS:", CLEAR + msg);
+        }
     }
 }
 
+function addChars(calc, string) {
+    string.split('').map( (c)=>calc.add_char(c) )
+    // console.log(calc.expression);
+}
 // TESTS
 // TESTS
 // TESTS
+
+console.log(BLACKONYELLOW + "\nTESTS",CLEAR);
 
 {
     const C = NewCalc()
     test("Default val of fresh instance is zero", C.equals, 0);
+}
+{
+    const C = NewCalc();
+    C.add_char("3");
+    test("Value of expression '3' is 3", C.equals, 3);
+}
+// display method needed for story #8
+{
+    const C = NewCalc();
+    addChars(C, "123+456");
+    test("Display of expression '123+456' is '123+456'", C.display, '123+456');
+}
+// Test for story #13 - no dupes except negatives
+{
+    const C = NewCalc();
+    addChars(C, "123+*456");
+    test("Display of expression '123+*456' is '123*456'", C.display, '123*456');
+}
+// Test numbers are being parsed correctly
+{
+    const C = NewCalc();
+    addChars(C, "123");
+    C.evaluate();
+    test("Test '123' is represented as [123] internally", C.expression, [123]);
+}
+// Test numbers are being parsed correctly
+{
+    const C = NewCalc();
+    addChars(C, "123+");
+    C.evaluate();
+    test("Test '123+' is represented as [123, '+'] internally", C.expression, [123,'+']);
+}
+{
+    const C = NewCalc();
+    addChars(C, "123+456");
+    C.evaluate();
+    // console.log(C.expression);
+    test("Test '123+456' is represented as [123, '+', 456] internally", C.expression, [123,'+',456]);
+}
+{
+    const C = NewCalc();
+    addChars(C, "12.3+456");
+    C.evaluate();
+    // console.log(C.expression);
+    test("Test '12.3+456' is represented as [12.3, '+', 456] internally", C.expression, [12.3,'+',456]);
+}
+//Test for story #13 - no dupes EXCEPT NEGATIVES
+{
+    const C = NewCalc();
+    addChars(C, "123+-456");
+    C.evaluate();
+    // console.log(C.expression);
+    test("Display of expression '123+-456' is '123+-456'", C.display, '123+-456');
+    test("Expression of '123+-456' is represented as [123, '+', -456]", C.expression, [123, '+', -456]);    
 }
